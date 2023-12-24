2ab7afc48951b75f1d708a178bb816ed39c24692
TheGhost <116256043+gamandeepsingh@users.noreply.github.com>
2023-12-25 00:07:24 +0530

    Add files via upload

---
+++ Day01/index.js
@@ -0,0 +1,46 @@
+// gsap.to("#box", {
+//     x:700,
+//     duration:2,
+//     delay:1,
+//     backgroundColor:"blue",
+//     rotate:360,
+//     scale:0.2
+// })
+
+// gsap.from("#box",{
+//     x:700,
+//     duration:2,
+//     delay:1,
+//     backgroundColor:"blue",
+//     rotate:360,
+//     scale:0.2
+// })
+
+// gsap.to("#h1",{
+//     x:"43%",
+//     color:"green",
+//     duration:3,
+//     delay:1
+// })
+// gsap.to("#h2",{
+//     x:"43%",
+//     color:"salmon",
+//     duration:3,
+//     delay:2
+// })
+// gsap.to("#h3",{
+//     x:"43%",
+//     color:"red",
+//     duration:3,
+//     delay:3
+// })
+
+// gsap.to("h1",{
+//     x:"43%",
+//     color:"green",
+//     duration:3,
+//     delay:1,
+//     stagger:1, //is used to add delay b/w same tags name
+//     repeat:1, // used to make repettition in the animation "-1" for infinte animation
+//     yoyo:true, // ease in and out (alternate)
+// })
---
+++ Day01/index2.js
@@ -0,0 +1,13 @@
+let tl = gsap.timeline()
+
+tl.to("h1",{
+    x:600,
+    delay:1
+})
+tl.to("h2",{
+    x:700,
+})
+tl.to("h3",{
+    x:800,
+})
+
